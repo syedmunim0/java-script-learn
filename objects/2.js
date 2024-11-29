5b8140e4ecb38ec9e762dfb39198068a5276f2a2
syedmunim0 <syedmohammadmunim@gmail.com>
2024-11-29 09:49:04 +0530

    json

---
+++ objects/2.js
@@ -0,0 +1,17 @@
+//Destructring in Js of objects
+const course = {
+    coursename: "Js in hindi",
+    price: "999",
+    courseinstructor:"muim",
+
+};
+
+
+
+// JSON talks
+// {
+//     //this str is know as json // also API
+
+// }
+// [ this str is also known as api
+//]
