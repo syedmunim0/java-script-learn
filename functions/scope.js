fcd1424295de6476c9779f9205c30d689b75e56d
syedmunim0 <syedmohammadmunim@gmail.com>
2024-12-05 20:57:00 +0530

    scopes aadha

---
+++ functions/scope.js
@@ -0,0 +1,14 @@
+//global and block scope 
+// {jo iske andar hoga that is ablock scope}//
+// y atab kaam aata hai jb bohat saare log one a single cheez kaam r rhe ho 
+//e.g
+
+//let a = 300
+// if (true){
+//   let a = 10
+//  const b = 20
+//     console.log('inner',a);// ye andar wala print hoga known as block scope 
+    
+// }
+//console.log(a);// ye bahr wala 300 print hoga known as global scope
+
