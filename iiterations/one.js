621fbc5e54f46886b20842e523e6acbabf37055e
syedmunim0 <syedmohammadmunim@gmail.com>
2024-12-07 09:50:30 +0530

    iiterations

---
+++ iiterations/one.js
@@ -0,0 +1,9 @@
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element)
+    
+}
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
---
+++ iiterations/two.js
@@ -0,0 +1,17 @@
+//While loop 
+
+let i = 0;
+
+while (i <= 10) {
+    //console.log(`value of i is ${i}`);
+    i = i + 2
+
+}
+let myArray = [`flash , batman , superman`]
+
+let arr = 0 
+while (arr < myArray.length) {
+    console.log(`value of arr is ${myArray[arr]}`);
+    arr = arr + 1;
+    
+}
